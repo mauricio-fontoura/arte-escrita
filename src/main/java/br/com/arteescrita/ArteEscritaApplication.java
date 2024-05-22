8de07adf291d5dfda5d726612a96cb2918496a86
mauricio-fontoura <mauriciofontour@gmail.com>
2024-05-22 16:00:30 -0300

    Projeto inicial v0.1

---
+++ src/main/java/br/com/arteescrita/ArteEscritaApplication.java
@@ -0,0 +1,13 @@
+package br.com.arteescrita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArteEscritaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArteEscritaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arteescrita/ArteEscritaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arteescrita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArteEscritaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
